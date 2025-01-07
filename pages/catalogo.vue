be134ea7d430dbe8c4b8b34ea88c0b707ace3ebb
Borja Cuerva <borja.cuerva@alebateducation.com>
2025-01-07 13:18:25 +0100

    chore(exercise): add exercise-1 solution

--- pages/catalogo.vue
+++ pages/catalogo.vue
@@ -4,9 +4,26 @@ definePageMeta({
 });
 </script>
 <template>
-  <NuxtLayout name="catalogue-layout">
-    <div>
-      <h1>Catálogo</h1>
-    </div>
-  </NuxtLayout>
+  <section>
+    <NuxtLayout name="catalogue-layout">
+      <div>
+        <h1>Catálogo</h1>
+      </div>
+      <NuxtLink class="button" :to="{ name: 'index' }"> HOME </NuxtLink>
+    </NuxtLayout>
+  </section>
 </template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
+.button {
+  padding: 1em 2em;
+  background-color: #fff;
+  color: #000;
+  width: fit-content;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>
-    <h1>HOME</h1>
-  </div>
+  <section>
+    <div>
+      <h1>HOME</h1>
+    </div>
+    <NuxtLink class="button" :to="{ name: 'catalogo' }"> CATÁLOGO </NuxtLink>
+  </section>
 </template>
+
+<style scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  gap: 1em;
+}
+.button {
+  padding: 1em 2em;
+  background-color: #fff;
+  color: #000;
+  width: fit-content;
+}
+</style>
